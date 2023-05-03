220902ad563c93a5376ffba426e3bddb1440be1c
Paul Villalta <paul.villalta@ucuenca.edu.ec>
2023-05-03 10:15:38 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-	// { path: 'login', loadChildren: () => import('./pages/auth/login/login.module').then(m => m.LoginModule) },
-	// { path: '', loadChildren: () => import('./layout/header-layout/header-layout.module').then(m => m.HeaderLayoutModule) },
-	{ path: '', loadChildren: () => import('./layout/sidebar-layout/sidebar-layout.module').then(m => m.SidebarLayoutModule) },
+	{ path: 'app', loadChildren: () => import('./layout/sidebar-layout/sidebar-layout.module').then(m => m.SidebarLayoutModule) },
+	{ path: '', loadChildren: () => import('./layout/header-layout/header-layout.module').then(m => m.HeaderLayoutModule) },
 ];
 
 @NgModule({
