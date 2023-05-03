fb6dc9c1a5157bf1399a30d70f491faba8183bae
Paul Villalta <paul.villalta@ucuenca.edu.ec>
2023-05-03 11:08:10 -0500

    router link

--- src/app/shared/components/sidebar/sidebar.module.ts
+++ src/app/shared/components/sidebar/sidebar.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar.component';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -10,6 +11,7 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
 	imports: [
 		CommonModule,
 		NzIconModule,
+		RouterModule,
 	]
 })
 export class SidebarModule { }
