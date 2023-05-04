e070be870dc20bc1cfa9e3c028da84a45a55c268
Samuel <samuel.pacurucu@ucuenca.edu.ec>
2023-05-04 14:09:08 -0500

    cambios BI

--- src/assets/paginas-samuel/JS/scriptBlogIndividual.js
+++ src/assets/paginas-samuel/JS/scriptBlogIndividual.js
@@ -13,9 +13,9 @@ imagenLike.addEventListener("click", function() {
 });
 
 imagenDislike.addEventListener("click", function() {
-  // Obtener el valor actual del contador y restar 1
+  // Obtener el valor actual del contador y sumar 1
   let valorActual = parseInt(Ndislikes.innerHTML);
-  let nuevoValor = valorActual - 1;
+  let nuevoValor = valorActual + 1;
   // Asignar el nuevo valor al contador
   Ndislikes.innerHTML = nuevoValor;
 });
@@ -41,6 +41,51 @@ function validarFormulario() {
     }
     
     // Enviar formulario si todos los campos son válidos
-    alert("Formulario enviado correctamente.");
+    alert("Comentario enviado correctamente.");
     document.querySelector("form").reset();
   }
+
+  
+  const comentarios = [
+    {
+        img:"../Imagen/usuario.svg",
+        autor: "Autor1",
+        fecha: "Abril 22, 2023 11:52:12 PM",
+        coment: "Contenido del comentario, este es un excelnete blog.........................",
+
+    },
+    {
+      img:"../Imagen/usuario.svg",
+      autor: "Autor 2",
+      fecha: "Abril 22, 2023 11:52:12 PM",
+      coment: "Contenido del comentario, este es un excelnete blog.........................",
+
+  },
+
+  {
+    img:"../Imagen/usuario.svg",
+    autor: "Autor X",
+    fecha: "Abril 22, 2023 11:52:12 PM",
+    coment: "Contenido del comentario, este es un excelnete blog.........................",
+
+},
+]
+
+let comment = document.getElementById("seccionComentarios");
+
+comentarios.map ((x) =>{
+  comment.innerHTML += `
+  <div class="comment">
+  <div class="comment-header">
+    <img src="${x.img}" alt="Avatar del autor">
+    <h4 class="comment-author">${x.autor}</h4>
+    <span class="comment-date">${x.fecha}</span>
+  </div>
+  <div class="comment-body">
+    <p>${x.coment}</p>
+  </div>
+</div>
+  `;
+}
+
+);
