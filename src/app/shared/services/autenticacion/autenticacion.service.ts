15a0f58d4cfd54a05da6e2609e1d9e70ba269e47
Paul Villalta <paul.villalta@ucuenca.edu.ec>
2023-07-20 22:59:35 -0500

    autenticacion

--- src/app/shared/services/autenticacion/autenticacion.service.ts
+++ src/app/shared/services/autenticacion/autenticacion.service.ts
@@ -13,4 +13,11 @@ export class AutenticacionService {
   registrar(usuario: any) {
     return this.http.post('/autenticacion/registro-usuario', usuario)
   }
+
+  login(email: string, clave: string) {
+    return this.http.post('/autenticacion/login', {
+      email: email,
+      clave: clave
+    })
+  }
 }
