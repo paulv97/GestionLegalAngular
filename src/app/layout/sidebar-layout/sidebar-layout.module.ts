dfa5163671203a78c735c47d7cb2c535d28c97d7
Paul Villalta <paul.villalta@ucuenca.edu.ec>
2023-07-22 21:14:37 -0500

    Guards para control de sesion

--- src/app/layout/sidebar-layout/sidebar-layout.module.ts
+++ src/app/layout/sidebar-layout/sidebar-layout.module.ts
@@ -3,14 +3,35 @@ import { CommonModule } from '@angular/common';
 import { SidebarLayoutComponent } from './sidebar-layout.component';
 import { RouterModule, Routes } from '@angular/router';
 import { SidebarModule } from 'src/app/shared/components/sidebar/sidebar.module';
+import { AuthGuard } from 'src/app/shared/guards/auth.guard';
 
 const routes: Routes = [
 	{ path: '', component: SidebarLayoutComponent, children: [
-		{ path: 'busqueda', loadChildren: () => import('../../pages/app/busqueda/busqueda.module').then(m => m.BusquedaModule) },
-		{ path: 'recursos', loadChildren: () => import('../../pages/app/recursos/recursos.module').then(m => m.RecursosModule) },
-		{ path: 'estadisticas', loadChildren: () => import('../../pages/app/estadisticas/estadisticas.module').then(m => m.EstadisticasModule) },
-		{ path: 'create-blog', loadChildren: () => import('../../pages/create-blog/create-blog.module').then(m => m.CreateBlogModule) },
-		{ path: 'documentos', loadChildren: () => import('../../pages/documentos/documentos.module').then(m => m.DocumentosModule) },
+		{ 
+			path: 'busqueda', 
+			loadChildren: () => import('../../pages/app/busqueda/busqueda.module').then(m => m.BusquedaModule), 
+			canActivate: [AuthGuard] 
+		},
+		{ 
+			path: 'recursos', 
+			loadChildren: () => import('../../pages/app/recursos/recursos.module').then(m => m.RecursosModule),
+			canActivate: [AuthGuard] 
+		},
+		{ 
+			path: 'estadisticas', 
+			loadChildren: () => import('../../pages/app/estadisticas/estadisticas.module').then(m => m.EstadisticasModule),
+			canActivate: [AuthGuard] 
+		},
+		{ 
+			path: 'create-blog', 
+			loadChildren: () => import('../../pages/create-blog/create-blog.module').then(m => m.CreateBlogModule),
+			canActivate: [AuthGuard] 
+		},
+		{ 
+			path: 'documentos', 
+			loadChildren: () => import('../../pages/documentos/documentos.module').then(m => m.DocumentosModule),
+			canActivate: [AuthGuard] 
+		},
 	] }
 ]
 
