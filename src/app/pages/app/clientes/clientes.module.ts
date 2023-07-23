83995b3ab43daa5a931ad084454c944d4b77a63b
Paul Villalta <paul.villalta@ucuenca.edu.ec>
2023-07-23 18:31:43 -0500

    tabla de clientes

--- src/app/pages/app/clientes/clientes.module.ts
+++ src/app/pages/app/clientes/clientes.module.ts
@@ -9,6 +9,8 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
 import { SearchPipeModule } from 'src/app/shared/pipes/search/search.module';
 import { NuevoJuicioModule } from '../busqueda/nuevo-juicio/nuevo-juicio.module';
 import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
 
 
 
@@ -26,6 +28,8 @@ import { NzModalModule } from 'ng-zorro-antd/modal';
 		SearchPipeModule,
 		NuevoJuicioModule,
 		NzModalModule,
+    NzTableModule,
+    NzDividerModule,
   ]
 })
 export class ClientesModule { }
