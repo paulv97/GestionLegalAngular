c80bc5ed9bc78df2ebb33e4be615a3faea1aa959
Paul Villalta <paul.villalta@ucuenca.edu.ec>
2023-07-24 22:49:22 -0500

    Dist GestionLegal

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/web-transaccional3'),
+      dir: require('path').join(__dirname, './coverage/GestionLegal'),
       subdir: '.',
       reporters: [
         { type: 'html' },
